9579904e74daf870347ea068e5de3708000361f0
Konstantin <ustinov.kn@mail.ru>
2023-07-27 23:20:29 +0300

    first commit

---
+++ src/main/java/com/bot/telegramweatherbot/TelegramWeatherBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.telegramweatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWeatherBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramWeatherBotApplication.class, args);
+    }
+
+}
